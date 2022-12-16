8d2f01d53cd4fa51f6d393fd8e571670b6b4c141
chechenec95rus <bagrov.saznew26@gmail.com>
2022-12-16 13:06:10 +0300

    Initial commit

---
+++ src/main/java/net/danilamaster/urpg/UnrealRPG.java
@@ -0,0 +1,15 @@
+package net.danilamaster.urpg;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UnrealRPG implements ModInitializer {
+	public static final String MOD_ID = "urpg";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/danilamaster/urpg/UnrealRPGClient.java
@@ -0,0 +1,10 @@
+package net.danilamaster.urpg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UnrealRPGClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
